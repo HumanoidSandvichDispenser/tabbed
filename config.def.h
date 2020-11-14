73e7f57f2d47b53999b6fff9ba7f37c1a7a65c54
Serhan Ekmekçi <serhanekmekci@protonmail.com>
2020-11-15 01:02:25 +0300

    config

--- config.def.h
+++ config.def.h
@@ -5,8 +5,7 @@ static const char font[]        = "Fira Code:size=12:antialias=true:autohint=tru
 static const char* normbgcolor  = "#ababab"; static const char* normfgcolor  = "#000000"; static const char* selbgcolor   = "#000000";
 static const char* selfgcolor   = "#ffffff";
 static const char* urgbgcolor   = "#111111";
-static const char* urgfgcolor   = "#cc0000";
-static const char before[]      = "<";
+static const char* urgfgcolor   = "#cc0000"; static const char before[]      = "<";
 static const char after[]       = ">";
 static const char titletrim[]   = "...";
 static const int  tabwidth      = 250;
@@ -34,8 +33,8 @@ static Bool npisrelative  = False;
 #define MODKEY Mod1Mask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-//	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
+	{ MODKEY,               XK_t,      spawn,       { 0 } },
 
 	{ MODKEY|ShiftMask,     XK_l,      rotate,      { .i = +1 } },
 	{ MODKEY|ShiftMask,     XK_h,      rotate,      { .i = -1 } },
@@ -43,7 +42,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,     XK_k,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
-	{ MODKEY,               XK_Return,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
+	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
 	{ MODKEY,               XK_1,      move,        { .i = 0 } },
 	{ MODKEY,               XK_2,      move,        { .i = 1 } },
 	{ MODKEY,               XK_3,      move,        { .i = 2 } },
@@ -55,7 +54,7 @@ static Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY,               XK_w,      killclient,  { 0 } },
 
 	{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	{ MODKEY|ShiftMask,     XK_u,      toggle,      { .v = (void*) &urgentswitch } },
